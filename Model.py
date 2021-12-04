3ac8d4c5d5ddfccc7ab729fd255bbb2d91bfab37
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2021-12-04 10:26:59 +0700

    Finishing the final UI

--- Model.py
+++ Model.py
@@ -1,55 +1,111 @@
 # Import module
 import tensorflow as tf
 import pickle
+import re
 
-# # Load vocab
-# file_to_read = open("vocab.pickle", "rb")
-# vocab = pickle.load(file_to_read)
-# file_to_read.close()
-
-# # Load model
-# model = tf.keras.models.load_model('my_sa_model')
-
-# def predict_sentiment(net, vocab, sentence):
-#     sentence = tf.constant(vocab[sentence.split()])
-#     label = tf.argmax(net(tf.reshape(sentence, (1, -1))), axis=1)
-#     if label == 1:
-#       return 'positive'
-#     elif label == 2:
-#       return 'negative'
-#     else:
-#       return 'neutral'
-
-# print(predict_sentiment(model, vocab, 'this movie is great')) # positive
-# print(predict_sentiment(model, vocab, 'this movie is so bad')) # negative
 
 class Model:
-  def __init__(self):
-    self.model = None
-    self.vocab = None
+    def __init__(self):
+        self.model = None
+        self.vocab = None
+        self.appos = {
+            "aren't": "are not",
+            "can't": "cannot",
+            "cant": "cannot",
+            "couldn't": "could not",
+            "didn't": "did not",
+            "doesn't": "does not",
+            "don't": "do not",
+            "hadn't": "had not",
+            "hasn't": "has not",
+            "haven't": "have not",
+            "he'd": "he would",
+            "he'll": "he will",
+            "he's": "he is",
+            "i'd": "I would",
+            "i'd": "I had",
+            "i'll": "I will",
+            "i'm": "I am",
+            "im": "I am",
+            "isn't": "is not",
+            "it's": "it is",
+            "it'll": "it will",
+            "i've": "I have",
+            "let's": "let us",
+            "mightn't": "might not",
+            "mustn't": "must not",
+            "shan't": "shall not",
+            "she'd": "she would",
+            "she'll": "she will",
+            "she's": "she is",
+            "shouldn't": "should not",
+            "that's": "that is",
+            "there's": "there is",
+            "they'd": "they would",
+            "they'll": "they will",
+            "they're": "they are",
+            "they've": "they have",
+            "we'd": "we would",
+            "we're": "we are",
+            "weren't": "were not",
+            "we've": "we have",
+            "what'll": "what will",
+            "what're": "what are",
+            "what's": "what is",
+            "what've": "what have",
+            "where's": "where is",
+            "who'd": "who would",
+            "who'll": "who will",
+            "who're": "who are",
+            "who's": "who is",
+            "who've": "who have",
+            "won't": "will not",
+            "wouldn't": "would not",
+            "you'd": "you would",
+            "you'll": "you will",
+            "you're": "you are",
+            "you've": "you have",
+            "'re": " are",
+            "wasn't": "was not",
+            "we'll": " will",
+            "didn't": "did not",
+            "gg": "going"
+        }
 
-  def load_vocab(self, filename="vocab.pickle"):
-    print("Loading vocabulary files.....")
-    file_to_read = open(filename, "rb")
-    self.vocab = pickle.load(file_to_read)
-    file_to_read.close()
+    def preprocess_text(self, sentence):
+        text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))',
+                      '', sentence)
+        text = re.sub('@[^\s]+', '', text)
+        text = text.lower().split()
+        reformed = [self.appos[word]
+                    if word in self.appos else word for word in text]
+        reformed = " ".join(reformed)
+        text = re.sub('&[^\s]+;', '', reformed)
+        text = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', text)
+        text = re.sub(' +', ' ', text)
+        return text.strip()
 
-  def load_the_model(self, modelname='my_sa_model'):
-    print("Loading model weights.....")
-    self.model = tf.keras.models.load_model(modelname)
+    def load_vocab(self, filename="vocab.pickle"):
+        print("Loading vocabulary files.....")
+        file_to_read = open(filename, "rb")
+        self.vocab = pickle.load(file_to_read)
+        file_to_read.close()
 
-  def predict(self, sentence):
-    sentence = tf.constant(self.vocab[sentence.split()])
-    label = tf.argmax(self.model(tf.reshape(sentence, (1, -1))), axis=1)
-    if label == 1:
-      return 'positive'
-    elif label == 2:
-      return 'negative'
-    else:
-      return 'neutral'
+    def load_the_model(self, modelname='my_sa_model'):
+        print("Loading model weights.....")
+        self.model = tf.keras.models.load_model(modelname)
 
-  def stupid_infer(self, sentence):
-    if (len(sentence.split()) > 1):
-      return 'positive'
-    else:
-      return 'negative'
+    def predict(self, sentence):
+        sentence = self.preprocess_text(sentence)
+        sentence = tf.constant(self.vocab[sentence.split()])
+        result = tf.squeeze(self.model(tf.reshape(sentence, (1, -1))))
+        return_dict = [('Neutral', result[0].numpy()), ('Positive',
+                                                        result[1].numpy()), ('Negative', result[2].numpy())]
+        return_dict.sort(key=lambda x: x[1], reverse=True)
+        return return_dict
+
+    def stupid_infer(self, sentence):
+        if (len(sentence.split()) > 1):
+            return 'positive'
+        else:
+            return 'negative'
--- ui.py
+++ ui.py
@@ -1,35 +1,72 @@
+from tkinter.constants import HORIZONTAL
 from Model import Model
-import tkinter
-    
+from tkinter import Entry, Tk, Label, Entry, Button, W, E
+from tkinter.ttk import Progressbar
+
+
 def main():
-    root = tkinter.Tk()
+    root = Tk()
+    root.title("Comment Sentiment Analysis")
     mymodel = Model()
     mymodel.load_vocab()
     mymodel.load_the_model()
     # print(mymodel.predict("I love watching movie"))
     # print(mymodel.stupid_infer("I love watching movie"))
 
-    e = tkinter.Entry(root, width=50)
-    e.pack()
+    label1 = Label(root, width=90)
+    label1['text'] = 'Enter your text'
+    label1.grid(row=0, column=0, columnspan=3, sticky=W+E, padx=2)
+
+    input = Entry(root, width=70)
+    input.grid(row=1, column=0, columnspan=2, sticky=W+E, padx=2)
 
     def predict_with_model():
-        sentence = e.get()
-        status_display['text'] = f'Your sentence: \'{sentence}\' is labeled as: '
-        # text_result['text'] = mymodel.stupid_infer(sentence)
-        text_result['text'] = mymodel.predict(sentence)
+        sentence = input.get()
+        status_display['text'] = f'Your input text is: '
+        displayText['text'] = sentence
+        result = mymodel.predict(sentence)
+        top1_label['text'] = f"{result[0][0]}: {result[0][1] * 100:.2f}%"
+        top2_label['text'] = f"{result[1][0]}: {result[1][1] * 100:.2f}%"
+        top3_label['text'] = f"{result[2][0]}: {result[2][1] * 100:.2f}%"
 
-    myButton = tkinter.Button(root, text="Predict", command=predict_with_model)
-    myButton.pack()
+        top1_chart['value'] = result[0][1] * 100
+        top2_chart['value'] = result[1][1] * 100
+        top3_chart['value'] = result[2][1] * 100
 
-    status_display = tkinter.Label(root)
+    myButton = Button(root, text="Predict",
+                      command=predict_with_model, width=20)
+    myButton.grid(row=1, column=2, sticky=E+W, padx=5, pady=5)
+
+    status_display = Label(root, width=90)
     status_display['text'] = 'Waiting for input'
-    status_display.pack()
+    status_display.grid(row=2, columnspan=3, column=0, sticky=E+W, padx=2)
 
-    text_result = tkinter.Label(root)
-    text_result['text'] = ''
-    text_result.pack()
+    displayText = Label(root, width=90)
+    displayText.grid(row=3, column=0, columnspan=3, sticky=E+W, padx=2)
+
+    top1_label = Label(root)
+    top1_label['text'] = 'Positive'
+    top1_label.grid(row=4, column=0, sticky=E+W, padx=2)
+
+    top1_chart = Progressbar(root, orient=HORIZONTAL)
+    top1_chart.grid(row=4, column=1, columnspan=2, sticky=W+E, padx=2)
+
+    top2_label = Label(root)
+    top2_label['text'] = 'Neutral'
+    top2_label.grid(row=5, column=0, sticky=E+W, padx=2)
+
+    top2_chart = Progressbar(root, orient=HORIZONTAL)
+    top2_chart.grid(row=5, column=1, columnspan=2, sticky=W+E, padx=2)
+
+    top3_label = Label(root)
+    top3_label['text'] = 'Negative'
+    top3_label.grid(row=6, column=0, sticky=E+W, padx=2)
+
+    top3_chart = Progressbar(root, orient=HORIZONTAL)
+    top3_chart.grid(row=6, column=1, columnspan=2, sticky=W+E, padx=2)
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
